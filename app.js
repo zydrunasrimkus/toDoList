4a4ed7f8ef30752957933fa76571ad494ec6b5d7
zydrunasrimkus <zydrunas.rimkus95@gmail.com>
2021-11-23 17:51:16 +0200

    prepared app port for heroku

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.post("/work", (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => {
-    console.log('Server up on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running!');
 });
